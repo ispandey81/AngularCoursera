1033a562f072287e3e310e0bb901ae79540d2984
indra <ispandey81@gmail.com>
2019-02-12 16:30:32 +0800

    Final assignment
    part 1 of the final assignment

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,12 +23,13 @@ export class HomeComponent implements OnInit {
   promotion: Promotion;
   leader: Leader;
   dishErrMsg: string;
+  promoErrMsg: string;
   constructor(private dishService: DishService, private promoService: PromotionService, private leaderService: LeaderService,
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
     this.dishService.getFeaturedDish().subscribe((dish) => this.dish = dish, errMess => this.dishErrMsg = <any>errMess);
-    this.promoService.getFeaturedPromotion().subscribe((promo) => this.promotion = promo);
+    this.promoService.getFeaturedPromotion().subscribe((promo) => this.promotion = promo, errMess => this.promoErrMsg = <any>errMess);
     this.leaderService.getFeaturedLeader().subscribe((leader) => this.leader = leader);
   }
 
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
 import { of, Observable } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, catchError, map } from 'rxjs/operators';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+import { baseURL } from '../shared/baseurl';
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PromotionService {
 
-  constructor() { }
+  constructor(private processHTTPMsgService: ProcessHTTPMsgService, private http: HttpClient) { }
   getPromotions(): Observable<Promotion[]> {
-    return of(PROMOTIONS).pipe(delay(2000));
+    // return of(PROMOTIONS).pipe(delay(2000));
+    return this.http.get<Promotion[]>(baseURL + 'promotions').pipe(catchError(this.processHTTPMsgService.handleError));
     // return new Promise(resolve => {
     //   // server delay simulation
     //   setTimeout(() => {
@@ -20,7 +25,8 @@ export class PromotionService {
     // return Promise.resolve(PROMOTIONS);
   }
   getPromotion(id: string): Observable<Promotion> {
-    return of(PROMOTIONS.filter((prom) => (prom.id === id))[0]).pipe(delay(2000));
+    // return of(PROMOTIONS.filter((prom) => (prom.id === id))[0]).pipe(delay(2000));
+    return this.http.get<Promotion>(baseURL + 'promotions/' + id).pipe(catchError(this.processHTTPMsgService.handleError));
     // return new Promise(resolve => {
     //   // server delay simulation
     //   setTimeout(() => {
@@ -30,7 +36,9 @@ export class PromotionService {
     // return Promise.resolve(PROMOTIONS.filter((prom) => (prom.id === id))[0]);
   }
   getFeaturedPromotion(): Observable<Promotion> {
-    return of(PROMOTIONS.filter((prom) => prom.featured)[0]).pipe(delay(2000));
+    // return of(PROMOTIONS.filter((prom) => prom.featured)[0]).pipe(delay(2000));
+    return this.http.get<Promotion>(baseURL + 'promotions?featured=true').pipe(map(promotions => promotions[0]))
+      .pipe(catchError(this.processHTTPMsgService.handleError));
     // return new Promise(resolve => {
     //   // server delay simulation
     //   setTimeout(() => {
