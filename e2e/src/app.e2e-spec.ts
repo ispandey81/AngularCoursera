68f6db0a9e4c747b87029825a6ab10088f9b7ce5
indra <ispandey81@gmail.com>
2019-02-19 12:08:08 +0800

    E2E
    Angular end to end testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,8 +9,26 @@ describe('workspace-project App', () => {
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to conFusion!');
+    page.navigateTo('/');
+    expect(page.getTitleText('app-root h1')).toEqual('Ristorante Con Fusion');
+  });
+
+  it('should navigate to about us page by clicking on a link', () => {
+    page.navigateTo('/');
+    let navlink = page.getAllElements('a').get(1);
+    navlink.click();
+    expect(page.getTitleText('h3')).toBe('About Us');
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+    const newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test author');
+    const newComment = page.getElement('textarea');
+    newComment.sendKeys('Test comment');
+    const newSubmitButton = page.getElement('button[type=submit]');
+    newSubmitButton.click();
+    browser.pause();
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,17 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(link: string) {
+    return browser.get(link);
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText();
+  getTitleText(selector: string) {
+    return element(by.css(selector)).getText();
+  }
+  getElement(selector: string) {
+    return element(by.css(selector));
+  }
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
   }
 }
